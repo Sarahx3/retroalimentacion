9a5303ef384138d281f0c77ce1848ac618cfee07
Aide Sarahi <aideflores5874@alumnos.udg.mx>
2020-09-17 17:18:22 -0500

    Completo opcion 1 y 2, falta estruc personaje

--- main.c
+++ main.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
 
+void capturar_enteros(){
+    int numeros[5],sum;
+    printf("Ingresa 5 numeros\n ");
+    for(size_t i=0; i<5; i++){
+        printf("%i.-", i+1);
+        scanf("%i", &numeros[i]); }
+    printf("Numeros ingresados: ");
+    for(size_t i=0; i<5; i++){
+        printf("%i ",numeros[i]);
+        sum+=numeros[i];}
+    printf("\nSuma: %i",sum);
+    printf("\nPromedio: %f\n",sum/5.0); 
+}
+
+void mostrar(int n, char cadena[]){
+    for (size_t i = 0; i < n; i++)
+    {
+        printf("%s",cadena);
+    }
+    
+
+}
+
 int main(){
     char op;
+    char cadena[20];
+    int n;
 
     do{
         printf("1) Capturar enteros.\n");
@@ -10,6 +35,27 @@ int main(){
         printf("4) Salir.\n");
         printf("Elige una opcion: ");
         scanf("%c",&op);
+
+        switch (op)
+        {
+        case '1':
+            capturar_enteros();
+            break;
+        case '2':
+            printf("Dame tu nombre: ");
+            fflush(stdin);
+            fgets(cadena, sizeof(cadena),stdin);
+            printf("n: ");
+            scanf("%i",&n);
+            mostrar(n, cadena);
+            break;
+        case '3':
+            break;
+
+        default:
+            break;
+        }
+
         fflush(stdin);
     }while (op!='4');
 
